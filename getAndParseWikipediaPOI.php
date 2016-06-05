8ad4a3c99e1266faf999adfef6db7aba35a1e861
sylvainar <sylvain.ar@hotmail.fr>
2016-06-05 23:06:29 +0200

    Changed the place of the title value

--- getAndParseWikipediaPOI.php
+++ getAndParseWikipediaPOI.php
@@ -21,6 +21,7 @@ function getAndParseWikipediaPOI($language, $user_latitude, $user_longitude, $ra
 	foreach ($apidata_array_geosearch as $currentPOI => $currentPOIdata) {
 		$wikipedia_pagesid_list .= '|'.$currentPOIdata['pageid'];
 		$output_array[$currentPOIdata['pageid']]['id'] = $currentPOIdata['pageid'];
+		$output_array[$currentPOIdata['pageid']]['name'] = $apidata_array_geosearch[$currentPOI]['title'];
 		$output_array[$currentPOIdata['pageid']]['latitude'] = $apidata_array_geosearch[$currentPOI]['lat'];
 		$output_array[$currentPOIdata['pageid']]['longitude'] = $apidata_array_geosearch[$currentPOI]['lon'];
 		$output_array[$currentPOIdata['pageid']]['distance'] = $apidata_array_geosearch[$currentPOI]['dist'];
